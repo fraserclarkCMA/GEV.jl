3bd8973e8427e7ccfd81309dc75a757b54c47473
Alan Crawford <a.crawford.11@ucl.ac.uk>
2019-08-23 12:39:01 +0100

    Add outer product approx for standard error

--- src/utils.jl
+++ src/utils.jl
@@ -11,3 +11,22 @@ end
 
 std_err(f::Function, x::Vector) = sqrt.(LinearAlgebra.diag(inv(ForwardDiff.hessian(f, x))))
 
+function std_err(beta::Vector{T}, cl::clogit) where T<:Real
+	J = length(beta)
+	OG = zeros(J,J)
+	for cld in cl.data 
+		g = analytic_grad_clogit_case(beta, cld) 
+		OG .+= g*g'
+	end
+	sqrt.(LinearAlgebra.diag(inv(OG)))
+end
+
+function std_err(theta::Vector{T}, nl::nlogit) where T<:Real
+	J = length(theta)
+	OG = zeros(J,J)
+	for id in 1:length(nl.data)
+		g = analytic_grad_nlogit_case(theta, nl, id) 
+		OG .+= g*g'
+	end
+	sqrt.(LinearAlgebra.diag(inv(OG)))
+end
